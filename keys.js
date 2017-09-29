05398be4647f666a942717e7dea033b27968a41c
Achea Compton <achea_10@hotmail.com>
2017-09-29 01:35:44 -0700

    almost

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded\n-------------\n');
+
+var twitterKeys = {
+  consumer_key: 'GZb8UktXowNfmNioVMLrgOzgK',
+  consumer_secret: '19PbcuEHNLLrtoHdvC7HbjX4layF8mAIu2oY77UvO8V4ti3dw1',
+  access_token_key: '715111262800052224-QYm0LsBTjSqT7b7V7n9PBbwJzUuAuR8',
+  access_token_secret: 'bzDrYJUslZohmT4Ul0vidaSEpOYQ22DLd80qQ740cZSUo',
+}
+
+var spotifyKeys = {
+	id:'3a79fb96ac3d47759c7bf0b9b7df4120',
+	secret:'38a2f3da79d04907bfe51cc48607c07c',
+}
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys
+}
