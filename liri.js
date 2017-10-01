2563f20633ab5dfa00f2de927c5af43f8f460fb4
Achea Compton <achea_10@hotmail.com>
2017-10-01 13:15:27 -0700

    almost there

--- liri.js
+++ liri.js
@@ -28,23 +28,60 @@ function switchCommand () {
 
 		default:
 			console.log("Invalid input try again"); 
-	}
-} 
+	};
+};
 
 var myTweets = function() {
 	var client = new Twitter (keys.twitterKeys);
-	var params = {}
+	var params = {screen_name: 'Acheaac', count 20};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (error) {
+      console.log('Twitter error' + error);
+    } 
+    else {
+		for (var i = 0; i < tweets.length; i++) {
+		console.log('Tweet: ' + tweets[i].text + '\n' + 'Created on: ' + tweets[i].created_at + '\n');}
+		}
 
-}
+});
 
 var spotifySong = function() {
-
-}
+	var spotify = new Spotify (keys.spoifiyKeys);
+	if (process.argv.length <3) {
+		spotify.search({type: 'track', query: 'The Sign Ace of Base'}, function (error, data) {
+			if (error) {
+				console.log(error);
+			}
+			else {
+				console.log (
+					"\nArtist: " + data.tracks.items[0].artist[0].name +
+					"\nTitle: " + data.tracks.items[0].name +
+					"\nPreview Link: " + data.tracks.items[0].preview_url +
+					"\nAlbum: " + data.tracks.items[0].album.name;
+					)
+			}
+		});
+	}
+};
 
 var movies = function() {
+	if (process.argv.length <3) {
+		var queryURL = "http://www.omdbapi.com/?apikey=40e9cece&t=mr+nobody";
+		request(queryURL, function(error, response, body) {
+			if (error) {
+				console.log(error);
+			}
+			else {
+				console.log(
+
+					)
+			}
+		})
+	}
 
 }
 
 var whatItSay = function() {
 
 }
+
